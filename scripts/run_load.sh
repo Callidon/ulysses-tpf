c74728a6cc7d81071345d69a692c40d3da9563fc
Thomas Minier <thomas.minier@protonmail.com>
2017-10-18 08:32:32 +0200

    update

--- scripts/run_load.sh
+++ scripts/run_load.sh
@@ -13,7 +13,7 @@ fi
 NBCLIENTS=(2 3 4 5 6 7 8 9 10 15 20 25 30 35 45 50 60 70 80 90 100)
 
 echo "clients,time" > $OUTPUT/execution_times.csv
-echo -n "1," >> $OUTPUT/execution_times.csv
+echo -n "0," >> $OUTPUT/execution_times.csv
 
 # run with only one client first
 ./scripts/run_with_time_ref.sh $FILE $OUTPUT
