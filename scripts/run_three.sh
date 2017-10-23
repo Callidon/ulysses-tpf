b954adf4c4c1080cef6159474e91a079ba6f4996
Thomas Minier <thomas.minier@protonmail.com>
2017-10-23 09:39:04 +0200

    try new version with config file

--- scripts/run_three.sh
+++ scripts/run_three.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 # run three times the experiment with each mode
 
-FILE=$1
-ROOT=$2
-CONFIG=$3
-LATENCY=$4
-QUERYFILE=$5
+source ./scripts/config.cfg
 
-if [ "$#" -ne 5 ]; then
-  echo "Illegal number of parameters."
-  echo "Usage: ./run_three.sh <file> <root-output> <ldf-server-config-file> <servers-latency> <query-file>"
-  exit
-fi
+# if [ "$#" -ne 5 ]; then
+#   echo "Illegal number of parameters."
+#   echo "Usage: ./run_three.sh <root-output> <ldf-server-config-file> <servers-latency> <query-file>"
+#   exit
+# fi
 
 # creates outputs directories
 
-mkdir -p $ROOT/output-ldf-run1/ $ROOT/output-ldf-run2/ $ROOT/output-ldf-run3/
-mkdir -p $ROOT/output-mixed-run1/ $ROOT/output-mixed-run2/ $ROOT/output-mixed-run3/
-mkdir -p $ROOT/output-peneloop-run1/ $ROOT/output-peneloop-run2/ $ROOT/output-peneloop-run3/
+mkdir -p $root_output/output-ldf-run1/ $root_output/output-ldf-run2/ $root_output/output-ldf-run3/
+mkdir -p $root_output/output-mixed-run1/ $root_output/output-mixed-run2/ $root_output/output-mixed-run3/
+mkdir -p $root_output/output-peneloop-run1/ $root_output/output-peneloop-run2/ $root_output/output-peneloop-run3/
 
 # run exp with ldf mode
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-ldf-run1/ ref
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-ldf-run2/ ref
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-ldf-run3/ ref
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-ldf-run1/ ref
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-ldf-run2/ ref
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-ldf-run3/ ref
 
 # run exp with peneloop mode
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-peneloop-run1/ peneloop
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-peneloop-run2/ peneloop
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-peneloop-run3/ peneloop
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-peneloop-run1/ peneloop
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-peneloop-run2/ peneloop
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-peneloop-run3/ peneloop
 
 # run exp with tpf+peneloop mode
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-mixed-run1/ tpf+peneloop
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-mixed-run2/ tpf+peneloop
-./scripts/launch_run.sh $CONFIG $LATENCY $QUERYFILE $ROOT/output-mixed-run3/ tpf+peneloop
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-mixed-run1/ tpf+peneloop
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-mixed-run2/ tpf+peneloop
+./scripts/launch_run.sh $ldf_config $latency $query_file $root_output/output-mixed-run3/ tpf+peneloop
