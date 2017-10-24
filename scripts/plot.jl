ce3d73c048700935fa4c69d17d3083a760af14eb
Thomas Minier <thomas.minier@protonmail.com>
2017-10-24 10:51:35 +0200

    add plot with avergae value + smooth shape for execution time with one query

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -6,29 +6,34 @@ using CalliPlots
 
 # Execution times
 
-time_ref = computeMean([
+time_ref = computeMean(
+    :time,
     "curio/run1/execution_times_ref.csv",
     "curio/run2/execution_times_ref.csv",
     "curio/run3/execution_times_ref.csv"
-]) do df
-    df[:approach] = "TPF"
-end
+)
+time_ref[:approach] = "TPF"
 
-time_peneloop = computeMean([
-    "curio/run1/execution_times.csv",
-    "curio/run2/execution_times.csv",
-    "curio/run3/execution_times.csv"
-]) do df
-    df[:approach] = "TPF + PeNeLoop"
-end
+time_peneloop = computeMean(
+    :time,
+    "curio/run1/execution_times_peneloop.csv",
+    "curio/run2/execution_times_peneloop.csv",
+    "curio/run3/execution_times_peneloop.csv"
+)
+time_peneloop[:approach] = "PeNeLoop only"
 
-# time_ref = readtable("curio/execution_times_ref.csv")
-# time_peneloop = readtable("curio/execution_times.csv")
-#
-# time_ref[:approach] = "TPF"
-# time_peneloop[:approach] = "TPF + PeNeLoop"
+time_mixed = computeMean(
+    :time,
+    "curio/run1/execution_times_mixed.csv",
+    "curio/run2/execution_times_mixed.csv",
+    "curio/run3/execution_times_mixed.csv"
+)
+time_mixed[:approach] = "TPF + PeNeLoop"
 
-p = plot([time_ref;time_peneloop], y=:mean_value, x=:clients, color=:approach, Geom.line, Guide.xlabel("Number of clients"), Guide.ylabel("Execution time (s)"), Scale.x_discrete)
+# RAW version
+# p = plot([time_ref;time_mixed;time_peneloop], y=:time, x=:clients, color=:approach, Geom.line, Guide.xlabel("Number of clients"), Guide.ylabel("Execution time (s)"), Scale.x_discrete)
+# Smooth version
+p = plot([time_ref;time_mixed;time_peneloop], y=:time, x=:clients, color=:approach, Geom.smooth(method=:loess, smoothing=0.9), Guide.xlabel("Number of clients"), Guide.ylabel("Execution time (s)"), Scale.x_discrete)
 
 # execution times plots
 draw(PDF("curio/execution_time.pdf", 15inch, 7inch), p)
