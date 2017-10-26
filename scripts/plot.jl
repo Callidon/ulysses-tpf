6b5a682ea57fdd8a6d9fedf2b8fcc185414613af
Thomas Minier <thomas.minier@protonmail.com>
2017-10-26 12:36:15 +0200

    update plots with 2 & 3servers

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -30,11 +30,30 @@ time_mixed = computeMean(
 )
 time_mixed[:approach] = "PeNeLoop + TPF"
 
+time_peneloop_3s = computeMean(
+    :time,
+    "curio/run1/execution_times_peneloop_3servers.csv",
+    "curio/run2/execution_times_peneloop_3servers.csv",
+    "curio/run3/execution_times_peneloop_3servers.csv"
+)
+time_peneloop_3s[:approach] = "PeNeLoop only"
+
+time_mixed_3s = computeMean(
+    :time,
+    "curio/run1/execution_times_mixed_3servers.csv",
+    "curio/run2/execution_times_mixed_3servers.csv",
+    "curio/run3/execution_times_mixed_3servers.csv"
+)
+time_mixed_3s[:approach] = "PeNeLoop + TPF"
+
 # RAW version
 # p = plot([time_ref;time_mixed;time_peneloop], y=:time, x=:clients, color=:approach, Geom.line, Guide.xlabel("Number of clients"), Guide.ylabel("Execution time (s)"), Scale.x_discrete)
 # Smooth version
 p = plot([time_ref;time_mixed;time_peneloop], y=:time, x=:clients, color=:approach, Geom.smooth(method=:loess, smoothing=0.75),
 Guide.xlabel("Number of clients executing the query in parallel"), Guide.ylabel("Execution time (s)"), Guide.colorkey("Type of clients"), Scale.x_discrete)
+p_3s = plot([time_ref;time_mixed_3s;time_peneloop_3s], y=:time, x=:clients, color=:approach, Geom.smooth(method=:loess, smoothing=0.75),
+Guide.xlabel("Number of clients executing the query in parallel"), Guide.ylabel("Execution time (s)"), Guide.colorkey("Type of clients"), Scale.x_discrete)
 
 # execution times plots
 draw(PDF("curio/execution_time_2servers.pdf", 15inch, 7inch), p)
+draw(PDF("curio/execution_time_3servers.pdf", 15inch, 7inch), p_3s)
